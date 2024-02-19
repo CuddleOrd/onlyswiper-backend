cea047dec0639cae7641de7468727782a16aaee0
truthfuldev <truthfuldev@gmail.com>
2024-02-19 19:19:14 +0100

    disable cors for test

--- src/index.ts
+++ src/index.ts
@@ -15,11 +15,11 @@ app.use(
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
 
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
+      // if (allowedOrigins.indexOf(origin) === -1) {
+      //   const msg =
+      //     "The CORS policy for this site does not allow access from the specified Origin.";
+      //   return callback(new Error(msg), false);
+      // }
 
       return callback(null, true);
     },
