12e416041936ed43967fec701cbe0e582ab6c5b0
Josphat Kipyegon <josphatkips@gmail.com>
2024-11-17 22:08:00 +0300

    updates on missiing routes

--- src/routes/creator.route.ts
+++ src/routes/creator.route.ts
@@ -6,6 +6,7 @@ import authMiddleware from "../middleware/auth.middleware.ts";
 const router = express.Router();
 
 router.post("/search", creatorController.search);
+router.post("/fetch-user",authMiddleware, creatorController.fetchUser);
 
 router.post(
   "/batch-create-by-scrapping",
